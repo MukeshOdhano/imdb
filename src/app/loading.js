454d0fc7c25cda6a0fef0d63e60474cf3f7c06f4
MukeshOdhano <mukeshsodhano@gmail.com>
2023-04-10 16:07:50 +0500

    Loading svg

---
+++ src/app/loading.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Loading() {
+  return (
+    <div className='h-96 flex justify-center items-center'>
+      <img src='loading.svg' alt='svg'/>
+    </div>
+  )
+}
